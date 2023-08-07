cfc22bb7f5bb1bf042aecb4d639467e3c8843d94
MarsIana1804 <139183555+MarsIana1804@users.noreply.github.com>
2023-08-07 19:51:29 +0200

    гзвфеу

--- home_work9.py
+++ home_work9.py
@@ -1,10 +1,37 @@
 
 user_data = {"Marina" : "+1234567890"}
+result = None
+
+def input_error(wrap):
+    def inner(*args):
+        try:
+            return wrap(*args)
+        except IndexError:
+            if len(*args) == 0:
+                print("Enter user name")
+            elif len(*args) == 1:
+                print("Enter BOTH user name and phone number")
+        except KeyError as k_e:
+            print(k_e)
+        except ValueError as v_e:
+            print(v_e)
+
+        except Exception as e:
+            print(e)
+            #return "Give name and phone number please"
+    return inner
 
 def hello_handler(*args):
     return "How can I help you?"
 
+
 def add_handler(string_input):
+
+    if string_input[0].isdigit():
+        print("KeyError")
+        raise KeyError("Number instead of name has been provided.Please enter the correct value.")
+    elif string_input[1].isalpha():
+        raise ValueError("Text instead of number has been provided.Please enter the correct phone number.")
 
     name = string_input[0].title()
     phone = string_input[1]
@@ -13,15 +40,26 @@ def add_handler(string_input):
     return f"Contact {name} with phone {phone} has been saved!"
 
 def get_phone_handler(string_input):
+
+    if string_input[0].isdigit():
+        #print("KeyError")
+        raise KeyError("Number instead of name has been provided. Please enter the correct value.")
+    elif string_input[1].isalpha():
+        raise ValueError("Text instead of number has been provided.Please enter the correct phone number.")
     name = string_input[0].title() 
-    phone = user_data.get(string_input[0])
-    if user_data.get(string_input[0]) != None:
+    phone = user_data.get(name)
+    if user_data.get(name) != None:
         
         return f"Here is the phone number of contact {name}: {phone}."
     else:
-        return f"User {string_input[0]} not found."
+        return f"User {name} not found."
 
 def change_handler(string_input):
+    if string_input[0].isdigit():
+        #print("KeyError")
+        raise KeyError("Number instead of name has been provided.Please enter the correct value.")
+    elif string_input[1].isalpha():
+        raise ValueError("Text instead of number has been provided.Please enter the correct phone number.")
     name = string_input[0].title()
     new_phone = string_input[1]
     user_data[name] = new_phone
@@ -37,41 +75,7 @@ def show_all_handler(*args):
 def exit_handler(*args):
     return "Good bye!"
 
-    
-# def get_command(string_input):
-#     command = string_input.split(" ")[0].lower()
-#     return command
 
-
-
-
-    print("An error occured:", e)
-
-
-
-
-
-
-
-# def input_error(wrap):
-#     def inner(*args):
-#         try:
-#             return wrap(*args)
-             
-#         except IndexError:
-#             return "Give name and phone number please"
-#     return inner
-
-
-
-
-
-#@input_error
-
-   
-    # name_phone = {string_input.split(" ")[1].title(): string_input.split(" ")[2]}
-    # user_data.update(name_phone)
-    return f"Contact {name} with phone {phone} has been saved!"
 
 
 commands = {
@@ -85,55 +89,38 @@ commands = {
 
 def command_parser(raw_str: str):
     elements = raw_str.split()
-    for key, value in commands.items():
-        for cmd in value:
-            if cmd.startswith(elements[0].lower()):
+    try:
+        for key, value in commands.items():
+            for cmd in value:
+                if cmd.startswith(elements[0].lower()):
+                    return key, elements[1:]
+            if elements[0].lower() in value:
                 return key, elements[1:]
-        if elements[0].lower() in value:
-            return key, elements[1:]
+    except Exception as e:
+        print(e)
 
 
-#@input_error
 def main():
     while True:
         user_input = input("Enter: ")
         if not user_input:
             continue
-        cmd, data = command_parser(user_input)
-        
-        result = cmd(data)
-        print(result)
+        try:
+            cmd, data = command_parser(user_input)
+            result = input_error(cmd)(data)
+            if result != None:
+                print(result)
 
-        if cmd == exit_handler:
-            break
-
-        #print(user_data)
+            if cmd == exit_handler:
+                break
 
 
-        # cmd = get_command(user_input)
-
-        
-
-        # elif cmd == "hello":
-        #     print("How can I help you?")
-
-        # elif user_input.lower() == "show all":
-        #     print(user_data)
-
-        
-
-        # elif cmd == "add":
-        #     add(user_input)
-
-        # elif cmd == "change":
-        #     change(user_input)        
-
-        # elif cmd == "phone":
-        #     get_phone(user_input)
+        except Exception as e:
+            result = None
+            print("Enter the correct command.")
 
         
     
-
 
 
 if __name__ == "__main__":
