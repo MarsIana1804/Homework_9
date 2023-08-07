9a55fb4186dfd5f3c32a50dd8a54879aa19efaf6
MarsIana1804 <139183555+MarsIana1804@users.noreply.github.com>
2023-08-07 16:58:24 +0200

    Update home_work9.py
    update

--- home_work9.py
+++ home_work9.py
@@ -1,62 +1,135 @@
 
 user_data = {"Marina" : "+1234567890"}
 
-def exit():
-    print("Good bye!")
-    
-def get_command(string_input):
-    command = string_input.split(" ")[0].lower()
-    return command
+def hello_handler(*args):
+    return "How can I help you?"
 
-def add(string_input):
+def add_handler(string_input):
 
-    try:
-        new_pair = {string_input.split(" ")[1]: string_input.split(" ")[2]}
-        user_data.update(new_pair)
+    name = string_input[0].title()
+    phone = string_input[1]
+    #email = string_input[3]
+    user_data[name] = phone
+    return f"Contact {name} with phone {phone} has been saved!"
 
-    except Exception as e:
-        print("An error occured:", e)
-
-
-def change(string_input):
-    user_data[string_input.split(" ")[1]] = string_input.split(" ")[2]
-
-def get_phone(string_input):
-    if user_data.get(string_input.split(" ")[1]) != None:
-        print(string_input.split(" ")[1], user_data.get(string_input.split(" ")[1]))
+def get_phone_handler(string_input):
+    name = string_input[0].title() 
+    phone = user_data.get(string_input[0])
+    if user_data.get(string_input[0]) != None:
+        
+        return f"Here is the phone number of contact {name}: {phone}."
     else:
-        print(f"User {string_input.split(' ')[1]} not found.")
+        return f"User {string_input[0]} not found."
+
+def change_handler(string_input):
+    name = string_input[0].title()
+    new_phone = string_input[1]
+    user_data[name] = new_phone
+    return f"Contact {name} has been changed to a new phone {new_phone}!"
+
+
+
+def show_all_handler(*args):
+    return user_data
+    
+
+
+def exit_handler(*args):
+    return "Good bye!"
+
+    
+# def get_command(string_input):
+#     command = string_input.split(" ")[0].lower()
+#     return command
 
 
 
 
-def bot():
+    print("An error occured:", e)
 
+
+
+
+
+
+
+# def input_error(wrap):
+#     def inner(*args):
+#         try:
+#             return wrap(*args)
+             
+#         except IndexError:
+#             return "Give name and phone number please"
+#     return inner
+
+
+
+
+
+#@input_error
+
+   
+    # name_phone = {string_input.split(" ")[1].title(): string_input.split(" ")[2]}
+    # user_data.update(name_phone)
+    return f"Contact {name} with phone {phone} has been saved!"
+
+
+commands = {
+    add_handler: ["add", "додай", "+"],
+    exit_handler: ["good bye", "close", "exit", "bye bye"],
+    hello_handler: ["hello", "hi"],
+    change_handler: ["change"],
+    show_all_handler: ["show all"],
+    get_phone_handler: ["phone"]
+}
+
+def command_parser(raw_str: str):
+    elements = raw_str.split()
+    for key, value in commands.items():
+        for cmd in value:
+            if cmd.startswith(elements[0].lower()):
+                return key, elements[1:]
+        if elements[0].lower() in value:
+            return key, elements[1:]
+
+
+#@input_error
+def main():
     while True:
+        user_input = input("Enter: ")
+        if not user_input:
+            continue
+        cmd, data = command_parser(user_input)
+        
+        result = cmd(data)
+        print(result)
 
-        user_input = input("Hello! ")
-        cmd = get_command(user_input)
-
-        if cmd in ["good bye", "close", "exit"]:
-            print("Good bye!")
+        if cmd == exit_handler:
             break
 
-        elif cmd == "hello":
-            print("How can I help you?")
+        #print(user_data)
 
-        elif user_input.lower() == "show all":
-            print(user_data)
+
+        # cmd = get_command(user_input)
 
         
 
-        elif cmd == "add":
-            add(user_input)
+        # elif cmd == "hello":
+        #     print("How can I help you?")
 
-        elif cmd == "change":
-            change(user_input)        
+        # elif user_input.lower() == "show all":
+        #     print(user_data)
 
-        elif cmd == "phone":
-            get_phone(user_input)
+        
+
+        # elif cmd == "add":
+        #     add(user_input)
+
+        # elif cmd == "change":
+        #     change(user_input)        
+
+        # elif cmd == "phone":
+        #     get_phone(user_input)
 
         
     
@@ -65,7 +138,7 @@ def bot():
 
 if __name__ == "__main__":
         
-        bot()
+        main()
 
 
 
